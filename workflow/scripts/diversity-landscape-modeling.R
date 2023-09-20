359d822bf3c9994c90c30dc40387c9d4ef3a7b41
Zachary Nolen <zachary.nolen@biol.lu.se>
2023-09-20 10:30:23 +0200

    Update diversity modeling code

--- workflow/scripts/diversity-landscape-modeling.R
+++ workflow/scripts/diversity-landscape-modeling.R
@@ -1,10 +1,11 @@
 sink(file(snakemake@log[[1]], open="wt"), type = "message")
 
-library(nlme)
+library(lme4)
+library(dplyr)
+library(car)
 library(MuMIn)
-library(ggplot2)
 
-
+# read in land cover and heterzozygosity data
 land_cover <- data.frame(
   read.table(snakemake@input[["cover"]], sep = "\t", header = TRUE)
 )
@@ -12,56 +13,56 @@ hz <- data.frame(
   read.table(snakemake@input[["hz"]], sep = "\t", header = TRUE)
 )
 outfile <- snakemake@output[["modelout"]]
+dataset <- snakemake@wildcards[["dataset"]]
+
+# define buffer radii around sample sites
 buffer_radius <- c(
   100, 500, 1000, 1500, 2000, 3000, 4000, 5000, 6000, 8000, 10000, 15000, 20000
 )
 
-
+# generate additional land use combination proportions
 hz$dataset <- snakemake@wildcards[["dataset"]]
 colnames(hz) <- c("sample", "site", "heterozygosity", "dataset")
 land_cover$grassland_forest <- land_cover$grassland + land_cover$forest
 land_cover$arable_grassland <- land_cover$arable + land_cover$grassland
 land_cover$arable_forest <- land_cover$arable + land_cover$forest
 
+# combine land use and heterozygosity dataframes
 ind_hz_land <- merge(hz, land_cover, by = "site")
 
 results <- data.frame()
 
+# set landscapes of interest
 landscapes <- c(
   "arable", "grassland", "forest", "water", "arable_grassland",
   "arable_forest", "grassland_forest"
 )
 
+# generate models of heterozygosity ~ landuse for each radii
 for (r in buffer_radius) {
   for (m in landscapes) {
     if (any(c(ind_hz_land[ind_hz_land$radius == r, m] > 0))) {
-      mod <- as.formula(sprintf("heterozygosity ~ %s", m))
-      model <- lme(
+      mod <- as.formula(sprintf("heterozygosity ~ %s + (1|site)", m))
+      model <- lmer(
         mod,
-        random = ~ 1 | site,
         data = ind_hz_land[ind_hz_land$radius == r, ]
       )
       summ <- summary(model)
       aic <- summ$AIC
-      r2 <- r.squaredGLMM(model)[1]
-      row <- c(
-        snakemake@wildcards[["dataset"]],
-        r,
-        m,
-        summ$tTable[8],
-        summ$tTable[10],
-        r2,
-        aic
-      )
+      tval <- summ$coefficients[6]
+      pval <- Anova(model)[[3]][1]
+      r2m <- r.squaredGLMM(model)[1]
+      r2c <- r.squaredGLMM(model)[2]
+      row <- c(dataset, r, m, tval, pval, r2m, r2c, aic)
       results <- rbind(results, row)
     }
   }
 }
 
-colnames(results) <- c(
-  "dataset", "radius", "landuse", "t-val", "p-val", "R2m", "aic"
-)
+colnames(results) <- c("dataset", "radius", "landuse", "t-val", "p-val", "r2m",
+                       "r2c", "aic")
 
+# write results to output table
 write.table(
   results,
   file = outfile,
