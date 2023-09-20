10f68a6d6b6e5d0cac88de0953371e3cda814ac6
Zachary Nolen <zachary.nolen@biol.lu.se>
2023-09-20 10:24:04 +0200

    Adding environment for landcape modeling

---
+++ workflow/envs/landscapemodels.post-deploy.sh
@@ -0,0 +1,6 @@
+# installs R package lmerMultiMember v0.11.4
+Rscript -e 'library(devtools)
+install_github(
+    "jvparidon/lmerMultiMember",
+    ref = "6f889ec9fb902287e5d2db62c46ebd67d655b713",
+    dependencies=FALSE)'
